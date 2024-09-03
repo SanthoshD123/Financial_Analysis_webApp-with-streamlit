048b6ad114efe8d90eb4d6f105dee7e83bf4466f
Santhosh D <141542516+SanthoshD123@users.noreply.github.com>
2024-09-03 20:04:06 +0530

    Update code .py

--- code .py
+++ code .py
@@ -1,3 +1,12 @@
 #Run in colab
 !pip install streamlit pandas yfinance pandas_datareader plotly
 !npm install -g localtunnel
+
+code = """
+import streamlit as st
+import pandas as pd
+import yfinance as yf
+import pandas_datareader.data as web
+import datetime
+import numpy as np
+import plotly.express as px"""
