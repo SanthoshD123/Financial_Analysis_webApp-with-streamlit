f33186652de02adfdcd34f7205af6f01acb00977
Santhosh D <141542516+SanthoshD123@users.noreply.github.com>
2024-09-04 21:26:24 +0530

    Update code .py

--- code .py
+++ code .py
@@ -10,3 +10,15 @@ import pandas_datareader.data as web
 import datetime
 import numpy as np
 import plotly.express as px"""
+
+# Function to plot interactive plotly chart
+def interactive_plot(df):
+    fig = px.line()
+    for i in df.columns[1:]:
+        fig.add_scatter(x=df['Date'], y=df[i], name=i)
+    fig.update_layout(
+        width=450,
+        margin=dict(l=20, r=20, t=20, b=20),
+        legend=dict(orientation='h', yanchor='bottom', y=1.02, xanchor='right', x=1)
+    )
+    return fig
